0256ceb3ad784568abfe04ad988978eb624df4a8
yoshiduels <yoshi.katreuvin@gmail.com>
2026-02-17 10:01:48 +0000

    fixed rush cards that return to hand/deck

--- script/rush/c160211049.lua
+++ script/rush/c160211049.lua
@@ -1,6 +1,6 @@
---超戦士の一撃
---Super Soldier Strike
---scripted by YoshiDuels
+--ラヴ・スタディ
+--Love Study
+--Scripted by YoshiDuels
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
@@ -9,31 +9,32 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCondition(s.condition)
-	e1:SetTarget(s.target)
-	e1:SetOperation(s.operation)
+	e1:SetTarget(s.tdtg)
+	e1:SetOperation(s.tdop)
 	c:RegisterEffect(e1)
 end
-s.listed_names={5405694}
-function s.cfilter(c)
-	return c:IsFaceup() and c:IsCode(5405694)
-end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil)
+	local g=Duel.GetMatchingGroup(Card.IsMonster,tp,LOCATION_GRAVE,0,nil)
+	local atk=g:GetSum(Card.GetAttack)
+	return #g>=6 and atk==0
 end
 function s.filter(c)
-	return c:IsFaceup() and not c:IsMaximumModeSide()
+	return not c:IsMaximumModeSide()
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.tdtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,0,LOCATION_ONFIELD,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,0,0)
 end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
+function s.tdop(e,tp,eg,ep,ev,re,r,rp)
 	--Effect
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
 	local g=Duel.SelectMatchingCard(tp,s.filter,tp,0,LOCATION_ONFIELD,1,1,nil)
 	if #g>0 then
 		Duel.HintSelection(g)
 		g=g:AddMaximumCheck()
-		Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
+		if Duel.SendtoDeck(g,nil,SEQ_DECKBOTTOM,REASON_EFFECT)<1 then return end
+		if #g>1 then
+			Duel.SortDeckbottom(1-tp,1-tp,#g)
+		end
 	end
 end
