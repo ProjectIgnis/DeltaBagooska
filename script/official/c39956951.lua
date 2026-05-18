796df95e16a7aeb5ff8b8576a13dd4f3998e4836
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2026-05-18 00:29:22 +0000

    Update "Flash of the Forbidden Spell"
    - Changed the filter/condition check to accommodate 3-column rules (e.g. Speed Duel)

---
+++ script/official/c39956951.lua
@@ -0,0 +1,34 @@
+--封魔一閃
+--Flash of the Forbidden Spell
+local s,id=GetID()
+function s.initial_effect(c)
+	--If your opponent controls monsters in all of their Main Monster Zones: Destroy all monsters your opponent controls.
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.filter(c)
+	local seq=c:GetSequence()
+	if Duel.IsDuelType(DUEL_3_COLUMNS_FIELD) then
+		return seq>0 and seq<4
+	else
+		return seq<5
+	end
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetMatchingGroupCount(s.filter,tp,0,LOCATION_MZONE,nil)==(Duel.IsDuelType(DUEL_3_COLUMNS_FIELD) and 3 or 5)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,tp,0,LOCATION_MZONE,1,nil) end
+	local sg=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_MZONE,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,sg,#sg,0,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local sg=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_MZONE,nil)
+	Duel.Destroy(sg,REASON_EFFECT)
+end
