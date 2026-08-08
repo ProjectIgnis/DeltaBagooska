db1c933bb2a13a261070d8ed1f3f3b4e02e6c684
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2026-08-08 04:34:51 +0000

    Update "Alchemy Beast" monsters
    Removed Special Summon restriction from "Alchemy Beast" monsters.

---
+++ script/unofficial/c100000653.lua
@@ -0,0 +1,12 @@
+--錬金獣・銅のウロボロス
+--Alchemy Beast - Ouroboros the Bronze
+local s,id=GetID()
+function s.initial_effect(c)
+	--Cannot be Normal Summoned/Set
+	c:EnableUnsummonable()
+	--This card can attack directly
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_DIRECT_ATTACK)
+	c:RegisterEffect(e2)
+end
---
+++ script/unofficial/c100000654.lua
@@ -0,0 +1,12 @@
+--錬金獣・鉛のレオーン
+--Alchemy Beast - Leon the Lead
+local s,id=GetID()
+function s.initial_effect(c)
+	--Cannot be Normal Summoned/Set
+	c:EnableUnsummonable()
+	--This card can attack directly
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_DIRECT_ATTACK)
+	c:RegisterEffect(e2)
+end
